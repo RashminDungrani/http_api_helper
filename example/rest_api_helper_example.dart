6296755746a3334d60157ab4e58c2c0e472d8c17
Rashmin Dungrani <rashmindungrani@gmail.com>
2023-05-24 12:58:04 +0530

    feat: 🎉 inital commit

---
+++ example/rest_api_helper_example.dart
@@ -0,0 +1,6 @@
+import 'package:rest_api_helper/rest_api_helper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/rest_api_helper.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/rest_api_helper_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
